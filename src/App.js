46ebaf8273f3d8c2b9d4db848d447c69ad850bfb
LauraAy <laura.ay1564@gmail.com>
2020-03-19 10:42:33 -0700

    following

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const App = () => {
-  return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  )
+class App extends Component {
+
+  state = {
+    numbers: [1, 2, 3, 4, 5]
+    number: null
+  }
+
+  handleInputChange = event => {
+    this.setState.apply({ [event.target.name]: event.target.value})
+  }
+
+  handleAddNumber = event => {
+    event.preventDefault()
+    let locNums = JSON.parse(JSON.stringify(this.state.numbers))
+    numbers.push(parseInt(this.state.number))
+    this.setState({ numbers: locNums, number: ''})
+  }
+  render() {
+    return (
+      <>
+      <form>
+        <p>
+          <label htmlFor="number">number</label>
+          <input
+          type="number"
+          name="number"
+          value={this.state.number}
+          onChange={this.handleInputChange}
+          />
+        </p>
+        <button onClick=
+      </form>
+      <h1>Hello World</h1>
+      <ul>
+
+      {this.state.numbers.map(x =><li>{x}</li>)}
+      </ul>
+     
+      </>
+    )
+  }
 }
 
 export default App
